c097e12233e19a448eb324c4fc51ebc0cd507d94
GilGuereque <gguereque326@gmail.com>
2023-08-13 21:18:38 -0700

    Setting up EJS

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ require('./config/passport')(passport)
 // Connect Database
 connectDB();
 
+// set up EJS
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger('dev'));
+
+// Session
+
 
 
 // Initial GET request
